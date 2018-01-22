e9de1e5952565da6453e3c341efe3b5c4f386b29
pfoldes <studio22@mail.ru>
2018-01-22 22:37:59 +0300

    Fixes #1
    - '>' and '<' characters now are replaced with spaces
    - done some extract method refactoring

--- src/Sanitizer.php
+++ src/Sanitizer.php
@@ -14,9 +14,11 @@ class Sanitizer
     {
         self::ensureString($query);
 
+        $reservedCharacters = self::reservedCharacters($exclude);
+
         return str_ireplace(
-            self::reservedCharacters($exclude),
-            array_keys(self::reservedCharacters($exclude)),
+            array_keys($reservedCharacters),
+            $reservedCharacters,
             $query
         );
     }
@@ -46,45 +48,83 @@ class Sanitizer
      */
     protected static function reservedCharacters(array $excludeCharacters = null)
     {
-        $reservedCharacters = [
-            "\\" => '\\',
-            '\+' => '+',
-            '\-' => '-',
-            '\=' => '=',
-            '\&\&' => '&&',
-            '\|\|' => '||',
-            '\>' => '>',
-            '\<' => '<',
-            '\!' => '!',
-            '\(' => '(',
-            '\)' => ')',
-            '\{' => '{',
-            '\}' => '}',
-            '\[' => '[',
-            '\]' => ']',
-            '\^' => '^',
-            '\"' => '"',
-            '\~' => '~',
-            '\*' => '*',
-            '\?' => '?',
-            '\:' => ':',
-            '\/' => '/',
-            '\A\N\D' => 'AND',
-            '\O\R' => 'OR',
-            '\N\O\T' => 'NOT',
-            '\ ' => ' '
-        ];
+        $reservedCharacters = self::prepareReservedCharacters();
 
         if (is_array($excludeCharacters)) {
-            $reservedCharacters = array_filter(
-                $reservedCharacters,
-                function ($reservedCharacter) use ($excludeCharacters) {
-                    return !in_array($reservedCharacter, $excludeCharacters, true);
-                }
-            );
+            $reservedCharacters = self::excludeReservedCharacters($reservedCharacters, $excludeCharacters);
         }
 
         return $reservedCharacters;
     }
 
+    /**
+     * Returns array of reserved characters and strings each symbol of which should be escaped with a backslash
+     *
+     * @return string[]
+     */
+    protected static function escapeWithSlashes()
+    {
+        return [
+            '\\', '+', '-', '=', '&&', '||', '!', '(', ')', '{', '}', '[',
+            ']', '^', '"', '~', '*', '?', ':', '/', 'AND', 'OR', 'NOT', ' '
+        ];
+    }
+
+    /**
+     * Returns array of reserved characters that should be replaced with a backslash-escaped space.
+     * Such characters cannot be escaped in an ElasticSearch query string at all, so the only option is to remove them.
+     *
+     * @see https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html#_reserved_characters
+     *
+     * @return string[]
+     */
+    protected static function replaceWithSpace()
+    {
+        return [
+            '>', '<'
+        ];
+    }
+
+    /**
+     * Returns an array where the keys are the reserved characters/strings and the values are the
+     * strings those characters/strings should be replaced/escaped with
+     *
+     * @return string[]
+     */
+    protected static function prepareReservedCharacters()
+    {
+        return array_merge(
+            array_combine(
+                self::escapeWithSlashes(),
+                array_map(
+                    function ($reservedString) {
+                        return implode('', preg_filter('/^/', '\\', str_split($reservedString)));
+                    },
+                    self::escapeWithSlashes()
+                )
+            ),
+            array_combine(
+                self::replaceWithSpace(),
+                array_fill(0, count(self::replaceWithSpace()), '\ ')
+            )
+        );
+    }
+
+    /**
+     * Excludes elements from the first array whose keys match an element in the second array
+     *
+     * @param string[] $reservedCharacters
+     * @param string[] $excludeCharacters
+     * @return string[]
+     */
+    protected static function excludeReservedCharacters(array $reservedCharacters, array $excludeCharacters)
+    {
+        return array_filter(
+            $reservedCharacters,
+            function ($reservedCharacter) use ($excludeCharacters) {
+                return !in_array($reservedCharacter, $excludeCharacters, true);
+            },
+            ARRAY_FILTER_USE_KEY
+        );
+    }
 }
--- tests/SanitizerTest.php
+++ tests/SanitizerTest.php
@@ -9,17 +9,19 @@ class SanitizerTest extends TestCase
     public function testSpecialCharactersEscaped()
     {
         $unescapedQuery = '+-=&&||><!(){}[]^"~*?\:/ AND OR NOT';
-        $expectedEscapedQuery = '\+\-\=\&\&\|\|\>\<\!\(\)\{\}\[\]\^\"\~\*\?\\\:\/\ \A\N\D\ \O\R\ \N\O\T';
+        $expectedEscapedQuery = '\+\-\=\&\&\|\|\ \ \!\(\)\{\}\[\]\^\"\~\*\?\\\\\:\/\ \A\N\D\ \O\R\ \N\O\T';
 
         $escapedQuery = Sanitizer::escape($unescapedQuery);
 
         $this->assertSame($expectedEscapedQuery, $escapedQuery);
+
+        var_dump($escapedQuery);
     }
 
     public function testExcludedCharactersNotEscaped()
     {
         $unescapedQuery = '+-=&&||><!(){}[]^"~*?\:/ AND OR NOT';
-        $expectedEscapedQuery = '+\-\=\&\&\|\|\>\<\!\(\){\}\[\]\^\"\~\*\?\\\:\/\ \A\N\D\ \O\R\ \N\O\T';
+        $expectedEscapedQuery = '+\-\=\&\&\|\|\ \ \!\(\){\}\[\]\^\"\~\*\?\\\\\:\/\ \A\N\D\ \O\R\ \N\O\T';
         $excludedCharacters = ['+', '{'];
 
         $escapedQuery = Sanitizer::escape($unescapedQuery, $excludedCharacters);
